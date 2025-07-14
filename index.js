af2d533504154865df10490a95e197b92959d8a8
meeravachhani <meeravachhani02.com>
2025-07-14 10:58:18 +0530

    odd even program

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("hello")
+const num1 = 7
+
+if(num1%2)
+{
+    console.log("even number")
+}
+else{
+    console.log("odd number")
+}
+
+// 2 maximum value
+
+console.log("maximum value")
+var a,b
+

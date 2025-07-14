5867d0d5d0f327181bc0f8a0cb1d9d521eabfa5c
meeravachhani <meeravachhani02.com>
2025-07-14 11:42:00 +0530

    today work complete

--- index.js
+++ index.js
@@ -23,15 +23,43 @@
 //     console.log("9")
 // }
 
-// 3 leap year
+// // 3 leap year
 
-console.log("leap year or not")
-const year=2020
-if(year/4)
-{
-    console.log("leap year")
-}
-else
-{
-    console.log("not leap year")
-}
+// console.log("leap year or not")
+// const year=2020
+// if(year/4)
+// {
+//     console.log("leap year")
+// }
+// else
+// {
+//     console.log("not leap year")
+// }
+
+// // 4 sum of number
+
+// const num=5
+// let sum=0
+// for(i=1;i<=5;i++)
+// {
+//     sum +=i
+//     // console.log("i:",i)
+// }
+// console.log(sum)
+
+// // 5 factorial of a number
+// const num=4
+// fact=1
+// for(i=1;i<5;i++)
+// {
+//     fact *=i
+    
+// }
+// console.log(fact)
+
+// // 6multiplication table
+// const num=3
+// for(i=1;i<=10;i++)
+// {
+//     console.log(num+"*"+i+"="+num*i)
+// }

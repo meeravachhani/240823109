148ebca3d01ab00979fa7ac85619b23fc66f254d
meeravachhani <meeravachhani02.com>
2025-07-14 11:30:13 +0530

    new changes

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-console.log("hello")
-const num1 = 7
+// console.log("hello")
+// const num1 = 7
 
-if(num1%2)
+// if(num1%2)
+// {
+//     console.log("even number")
+// }
+// else{
+//     console.log("odd number")
+// }
+
+// // 2 maximum value
+
+// console.log("maximum value")
+// const a=5
+// const b=9
+
+// if(a>b)
+// {
+//     console.log("5")
+// }
+// else{
+//     console.log("9")
+// }
+
+// 3 leap year
+
+console.log("leap year or not")
+const year=2020
+if(year/4)
 {
-    console.log("even number")
+    console.log("leap year")
 }
-else{
-    console.log("odd number")
+else
+{
+    console.log("not leap year")
 }
-
-// 2 maximum value
-
-console.log("maximum value")
-var a,b
-
-de

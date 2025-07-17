6c46c382233f70ca08eb9684919b483f49772a04
meeravachhani <meeravachhani02@gmail.com>
2025-07-17 09:00:31 +0530

    today work

--- index.js
+++ index.js
@@ -64,12 +64,73 @@
 //     console.log(num+"*"+i+"="+num*i)
 // }
 
-// 7
-for ( i =4; i >= 1; i--) {
-    console.log(i);
-  }
+// // 7
+// for ( i =4; i >= 1; i--) {
+//     console.log(i);
+//   }
   
-// 20
-const a=2
-const b=3
-console.log(a**b)
+// // 11
+// var sum=0
+// for(i=1;i<=3;i++)
+// {     
+//     console.log("sum of number",i)
+//     sum+=i
+// }
+// console.log(sum)
+
+// // 14
+// const str='o'
+// if(str==='a'||str==='e' ||str==='i' ||str==='o' ||str==='u' ||str==='A' ||str==='E' ||str==='O' ||str==='I' ||str==='U' )
+// {
+//     console.log("vowel")
+
+// }
+// else
+// {
+//     console.log("consonant")
+// }
+
+
+// // 15
+// const num1=4
+// const num2=2
+// console.log("addition of two value",num1+num2)
+// console.log("subtraction of two value",num1-num2)
+// console.log("multiplication of two value",num1*num2)
+// console.log("divistion of two value",num1/num2)
+
+
+// // 18
+// const number = 10; 
+
+// for (let i = 1; i <= number; i++) 
+// {
+//   if (number % i === 0) 
+//   {
+//     console.log(i)
+//   }
+// }
+
+
+// // 19
+// const num=5
+// if(num==0)
+// {
+//     console.log("zero")
+    
+// }
+//     if (num>0) 
+//     {
+//     console.log("positive")
+        
+//     } 
+//     else 
+//     {
+//         console.log("negative")
+//     }
+
+// // 20
+// const a=2
+// const b=3
+// console.log(a**b)
+
